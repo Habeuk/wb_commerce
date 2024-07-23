e4af2f9825e3148b475efb14ecb0d11434938dba
vystii <ngahabanda@gmail.com>
2024-07-23 18:48:33 +0200

    ajout du plugin d'affichage des moyens de paiement dans le dashbord

---
+++ src/Plugin/ManageModuleConfig/ManagePaiement.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Drupal\wb_commerce\Plugin\ManageModuleConfig;
+
+use Drupal\manage_module_config\ManageModuleConfigPluginBase;
+use Drupal\Core\Url;
+
+/**
+ * Gestion du menu.
+ *
+ * @ManageModuleConfig(
+ *   id = "manage_paiement",
+ *   label = @Translation("Manage paiement"),
+ *   description = @Translation("Foo description.")
+ * )
+ */
+class ManagePaiement extends ManageModuleConfigPluginBase {
+
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\manage_module_config\ManageModuleConfigInterface::GetName()
+   */
+  public function GetName() {
+    return $this->configuration['name'];
+  }
+
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\manage_module_config\ManageModuleConfigInterface::getRoute()
+   */
+  public function getRoute() {
+    /**
+     *
+     * @var \Drupal\Core\Http\RequestStack $RequestStack
+     */
+    $RequestStack = \Drupal::service('request_stack');
+    $Request = $RequestStack->getCurrentRequest();
+    $url = (\Drupal::moduleHandler()->moduleExists('lesroidelareno')) ?
+      Url::fromRoute('lesroidelareno.payement_gateways', [
+        'payment_plugin_id' => 'list-all'
+      ], [
+        'query' => [
+          'destination' => $Request->getPathInfo()
+        ]
+      ]) :
+      Url::fromRoute('entity.commerce_payment_gateway.collection', [], [
+        'query' => [
+          'destination' => $Request->getPathInfo()
+        ]
+      ]);
+    return $url;
+  }
+
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\manage_module_config\ManageModuleConfigInterface::getDescription()
+   */
+  public function getDescription() {
+    return $this->configuration['description'];
+  }
+
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\manage_module_config\ManageModuleConfigPluginBase::defaultConfiguration()
+   */
+  public function defaultConfiguration() {
+    return [
+      'name' => 'Moyens de paiement',
+      'description' => "Activer et configurer les moyens de paiements",
+      'enable' => false,
+      'icon_svg_class' => 'btn-info text-white btn-lg',
+      'icon_svg' => '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M310.706 413.765c-1.314-6.63-7.835-10.872-14.424-9.369-10.692 2.439-27.422 5.413-45.426 5.413-56.763 0-101.929-34.79-121.461-85.449h113.689a12 12 0 0 0 11.708-9.369l6.373-28.36c1.686-7.502-4.019-14.631-11.708-14.631H115.22c-1.21-14.328-1.414-28.287.137-42.245H261.95a12 12 0 0 0 11.723-9.434l6.512-29.755c1.638-7.484-4.061-14.566-11.723-14.566H130.184c20.633-44.991 62.69-75.03 117.619-75.03 14.486 0 28.564 2.25 37.851 4.145 6.216 1.268 12.347-2.498 14.002-8.623l11.991-44.368c1.822-6.741-2.465-13.616-9.326-14.917C290.217 34.912 270.71 32 249.635 32 152.451 32 74.03 92.252 45.075 176H12c-6.627 0-12 5.373-12 12v29.755c0 6.627 5.373 12 12 12h21.569c-1.009 13.607-1.181 29.287-.181 42.245H12c-6.627 0-12 5.373-12 12v28.36c0 6.627 5.373 12 12 12h30.114C67.139 414.692 145.264 480 249.635 480c26.301 0 48.562-4.544 61.101-7.788 6.167-1.595 10.027-7.708 8.788-13.957l-8.818-44.49z"/></svg>'
+    ] + parent::defaultConfiguration();
+  }
+}
